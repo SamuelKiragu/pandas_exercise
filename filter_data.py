47debc18b8afb8af0586e0162bfd902101181c6f
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-22 16:26:22 +0300

    Added funtionality to filter data

--- filter_data.py
+++ filter_data.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
-def filter_data(filepath):
-    dataframe = pd.read_csv(filepath) # Read data frame
+# filepath: path of the data file
+# start: index to start slicing operation
+# n: number of rows to be sliced
+def filter_data(filepath, start, n):
+    filtered_data = []
+    df = pd.read_csv(filepath) # Read data frame
+    df = df.iloc[start: start+n] # Split the data
 
-    #TODO: Extract Data from columns
-    
-    return dataframe
+    for row in df.values:
+        dict = {k:v for k,v in zip(df.columns, row)}
+        filtered_data.append(dict)
+
+    return filtered_data
