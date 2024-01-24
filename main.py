72b08921880b66667a32bb3bf9b88e528ff2ab3d
Samuel Kiragu <samuelkiragu107@gmail.com>
2024-01-24 15:59:15 +0300

    Added tests for methods filter_males and filter_females

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ dataframe_path = './Churn_Modelling.csv' # CSV File
 start = 2 # Start Slice here
 rows = 10 # No of rows to return
 
-print(f.filter_data(dataframe_path, start, rows))
+# Test function filter_data
+# print(f.filter_data(dataframe_path, start, rows))
+
+# Test function filter_female
+print(f.filter_females(dataframe_path))
+
+# Test function filter_male
+print(f.filter_males(dataframe_path))
